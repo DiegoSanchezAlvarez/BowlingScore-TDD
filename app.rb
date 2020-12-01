57e01dfe6abe8063666b17df9692eb30af36a681
DiegoSanchezAlvarez <diego.sanchez@ucb.edu.bo>
2020-12-01 00:06:11 -0400

    Ver4 | Ya se visualiza los puntajes del primer Frame de forma muy basica y refactorizable

--- app.rb
+++ app.rb
@@ -1,7 +1,29 @@
 require 'sinatra'
 require './config'
+require './lib/frame.rb'
+
+frameBowling = Frame.new()
 
 get '/' do
     'hola mundo'
+    @roll1 = frameBowling.obtenerPuntaje_Del_Primer_Tiro()
+    @roll2 = frameBowling.obtenerPuntaje_Del_Segundo_Tiro()
+    @score_frame = frameBowling.obtenerPuntaje_Del_Frame()
+    erb :tablero
+end
+
+post '/tiro1' do
+    frameBowling.lanzamiento_Del_Primer_Tiro(2)
+    @roll1 = frameBowling.obtenerPuntaje_Del_Primer_Tiro()
+    @roll2 = frameBowling.obtenerPuntaje_Del_Segundo_Tiro()
+    @score_frame = frameBowling.obtenerPuntaje_Del_Frame()
+    erb :tablero
+end
+
+post '/tiro2' do
+    frameBowling.lanzamiento_Del_Segundo_Tiro(5)
+    @roll1 = frameBowling.obtenerPuntaje_Del_Primer_Tiro()
+    @roll2 = frameBowling.obtenerPuntaje_Del_Segundo_Tiro()
+    @score_frame = frameBowling.obtenerPuntaje_Del_Frame()
     erb :tablero
 end
